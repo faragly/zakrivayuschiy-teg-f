a3f7b236390b3171f080fb11f94871d33d67ec29
Khalik Faradzhli <khalik.faradzhli@mail.ru>
2024-09-29 21:47:50 +0300

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import stylelint from 'vite-plugin-stylelint';
 
 export default defineConfig({
+  base: '/zakrivayuschiy-teg-f/',
   plugins: [
     stylelint({
       fix: true,
