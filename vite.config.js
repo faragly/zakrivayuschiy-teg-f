d51d7c5645239872daa6510ffddda3aaa8ff90ff
Khalik Faradzhli <khalik.faradzhli@mail.ru>
2024-10-12 18:18:41 +0300

    fixed fonts

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite';
 import stylelint from 'vite-plugin-stylelint';
 
 export default defineConfig({
   base: '/zakrivayuschiy-teg-f/',
+  resolve: {
+    alias: {
+      '@fonts': resolve(__dirname, 'src/fonts'),
+    }
+  },
   plugins: [
     stylelint({
       fix: true,
